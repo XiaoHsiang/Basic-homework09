421a52da854566aa956da85b44c9b8def89984ec
XiaoHsiang <48340410+XiaoHsiang@users.noreply.github.com>
2019-03-29 10:23:05 +0800

    Add files via upload

---
+++ Basic-homework09.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+	int i,j;
+	for (i=1;i<=9;i++)
+	{
+	for(j=1;j<=9;j++)
+	printf("%d*%d=%2d  ",j,i,j*i);
+	printf("\n");
+    }
+	system("pause");
+	return 0;
+}
